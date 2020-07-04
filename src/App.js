1b340981c4227aca17d2916b15fd2379dec40685
cientista1 <40244138+cientista1@users.noreply.github.com>
2020-07-03 23:37:28 -0300

    Estrutura Final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Routes from "./routes";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Header />
         <Routes />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
